128aea5d413ff61533f132f9e7ac8f718bcac4f2
Gleb Bezborodov <bezborodoff.gleb@gmail.com>
2021-10-31 19:44:35 +0700

    some improvements in tests

--- test/object.cpp
+++ test/object.cpp
@@ -6,9 +6,19 @@
  * this file. If not, please write to: bezborodoff.gleb@gmail.com, or visit : https://github.com/glensand/e-rock-json
  */
 
-#include "erock/strict/types.h"
-#include "gtest/gtest.h"
+#include "erock/strict/document.h"
 
-TEST(Initial, object){
-    erock::e_int_t test_o{"Name"};
+#include "gtest/gtest.h"
+#include <iostream>
+
+struct simple_struct{
+    erock::int_t v1{"field1"};
+    erock::bool_t v2{"field2"};
+    erock::real_t v3{"field3"};
+};
+
+TEST(Initial, simple_struct){
+    using doc_t = erock::object<simple_struct>;
+    doc_t instance;
+    auto&& object = erock::load<doc_t>("test.json");
 }
